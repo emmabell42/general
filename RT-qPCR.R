e2fdacb4eef4a16b54bb54e0ff586a2545f2a29f
emmabell42 <emmabell42@gmail.com>
2017-04-20 19:52:33 +0100

    Update RT-qPCR.R

--- RT-qPCR.R
+++ RT-qPCR.R
@@ -2,57 +2,42 @@ setwd("\\\\store.ic.ac.uk\\IC\\fom\\surgeryandcancer\\epigenetics-and-developmen
 ddct <- read.table("ddcts.txt",sep="\t",stringsAsFactors=F,head=F,row.names=1)
 colnames(ddct) <- ddct[1,]
 ddct <- ddct[2:nrow(ddct),]
-logfcs <- read.table("logfc.txt",sep="\t",stringsAsFactors=F,head=F,row.names=1)
-colnames(logfcs) <- labs
+logfcs <- read.table("logfcs.txt",sep="\t",stringsAsFactors=F,head=F,row.names=1)
+colnames(logfcs) <- c("Target",colnames(ddct),"Col")
 sems <- read.table("sems.txt",sep="\t",stringsAsFactors=F,head=F,row.names=1)
-geomean = function(x, na.rm=TRUE, zero.propagate = FALSE){
-  if(any(x < 0, na.rm = TRUE)){
-    return(NaN)
-  }
-  if(zero.propagate){
-    if(any(x == 0, na.rm = TRUE)){
-      return(0)
-    }
-    exp(mean(log(x), na.rm = na.rm))
-  } else {
-    exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
-  }
-}
-toReplace <- c("_Sample.*","_p.*")
-labs <- unique(gsub(paste0(toReplace,collapse="|"),"",colnames(ddct)))
-geomeans <- array(NA,dim=c(nrow(ddct),length(labs)))
-colnames(geomeans) <- labs
-rownames(geomeans) <- rownames(ddct)
-sems <- geomeans
-
-for(i in 1:ncol(geomeans)){
-toCalc <- labs[i]
-	for(j in 1:nrow(geomeans)){
-	values <- as.numeric(ddct[j,grep(toCalc,colnames(ddct))])
-	geomeans[j,i] <- geomean(values)
-	sems[j,i] <- sd(log2(values))/sqrt(length(values))
-	}
-}
-
+colnames(sems) <- colnames(ddct)
 error.bar <- function(x, y, upper, lower=upper, length=0.1,...){
 if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
 stop("vectors must be same length")
 arrows(x,y+upper, x, y-lower, angle=90, code=3, length=length, ...)
 }
 
-arrows(x0, y0, x1 = x0, y1 = y0
-x <- barplot(as.numeric(logfcs[1,]))
-for(i in 1:nrow(logfcs)){
-png(paste0(rownames(logfcs)[i],"_log2FC.png"))
-ylim <- c(min(na.omit(as.numeric(logfcs[i,])-as.numeric(sems[i,]))),max(na.omit(as.numeric(logfcs[i,])+as.numeric(sems[i,]))))*1.1
-barplot(as.numeric(logfcs[i,]),las=2,ylab="Log2(FC)",main=rownames(logfcs)[i],ylim=ylim,names=labs,cex.names=0.7)
-y <- as.numeric(logfcs[i,])
-y[is.na(y)] <- 0
-semstoplot <- as.numeric(sems[i,])
+se <- c("lightgreen","pink")
+
+x <- barplot(toplot[grep(paste0(se,collapse="|"),toplot[,17]),])
+for(i in c(2,4,7,8,9,11,13,15)){
+png(paste0(colnames(logfcs)[i],"_se_log2FC.png"))
+toplot <- logfcs[order(logfcs[,i]),]
+toplot <- toplot[grep(paste0(se,collapse="|"),toplot[,17]),]
+semstoplot <- as.numeric(sems[,i-1])
+semstoplot <- semstoplot[order(logfcs[,i])]
+semstoplot <- semstoplot[grep(paste0(se,collapse="|"),toplot[,17])]
 semstoplot[which(semstoplot==0)] <- NA
 semstoplot[is.na(semstoplot)] <- 0.001
+ylim <- c(min(na.omit(as.numeric(toplot[,i])-as.numeric(semstoplot))),max(na.omit(as.numeric(toplot[,i])+as.numeric(semstoplot))))*1.1
+barplot(toplot[,i],las=2,ylab="Log2(FC)",main=colnames(logfcs)[i],ylim=ylim,names=toplot[,1],col=toplot[,17],cex.names=0.8)
+y <- as.numeric(toplot[,i])
+y[is.na(y)] <- 0
 arrows(as.numeric(x),y-semstoplot,x,y+semstoplot,length=0.05, angle=90, code=3)
 grid(nx=NA,ny=NULL)
-abline(v=c(4.9,8.5,10.9,13.3,15.7),col = "lightgray", lty = "dotted")
+abline(v=c(6.1,12.1,18.1,24.1),col = "lightgray", lty = "dotted")
+dev.off()
+}
+
+for(i in c(2,4,7,8,9,11,13,15)){
+png(paste0(colnames(logfcs)[i],"_boxplot_log2FC.png"),width=240)
+toplot <- logfcs[order(logfcs[,i]),]
+toplot <- toplot[grep(paste0(se,collapse="|"),toplot[,17]),]
+boxplot(toplot[,i]~toplot[,17],ylab="Log2(FC)",main=colnames(logfcs)[i],names=c("Maintained","Silenced"),col=c("lightgreen","pink"),cex.names=0.8)
 dev.off()
 }
